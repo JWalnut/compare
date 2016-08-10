a95d9a114a12b8814d9226b4416d3e1d618b931d
John Walnut <jw5az@virginia.edu>
2016-08-10 11:11:09 -0400

    Added more documentation and implemented PCA in DataScience

--- compare/func/datascience.py
+++ compare/func/datascience.py
@@ -4,14 +4,28 @@
 # 8/2/16
 
 import numpy.linalg as np
+import numpy
 
 def pca(simMatrix, k=0):
-    U, s, V = np.svd(simMatrix)
-    print "U = ", U
-    U_k = U[:, 0:k+1]
-    print "U_k = ", U_k
-    X = U_k.transpose().dot(simMatrix)
-    print "X = ", X
-    Z = U_k.dot(X)
-    print "Z = ", Z
+    # Center Data
+    #mean = simMatrix.mean(axis=0)
+    #print "Means = ", mean
+    #simMatrix = simMatrix - mean
+    #print "Centered = ", simMatrix
+    #U, s, V = np.svd(simMatrix)
+    #print "Eigenvectors = ", U
+    #U_k = V[:, 0:k+1]
+    #print "1st Eigenvector = ", U_k
+    #X = U_k.transpose()
+    #X = X.dot(simMatrix.transpose())
+    #print "X = ", X
+    #Z = U_k.dot(X)
+    #print "Z = ", Z
+    #return Z
+    
+    simMatrix = simMatrix - simMatrix.mean(axis=0)
+    cov = numpy.cov(simMatrix.T)
+    x1, x2, U = np.svd(cov)
+    evecs = U[:, 0:k+1]
+    Z = evecs.T.dot(simMatrix.T)
     return Z
--- documentation/index.rst
+++ documentation/index.rst
@@ -16,6 +16,7 @@ Contents:
    wordmap
    document   
    ngram
+   corpus
 
 Indices and tables
 ==================
