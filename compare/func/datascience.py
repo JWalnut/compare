880fc14faa32f758a008b93163012fcf0fec4ef0
John Walnut <jw5az@virginia.edu>
2017-01-10 15:52:11 -0500

    Implemented MMDS function

--- compare/func/datascience.py
+++ compare/func/datascience.py
@@ -5,6 +5,7 @@
 
 import numpy.linalg as np
 import numpy
+import math
 
 # pca based on the covariance matrix
 def pca(simMatrix, k=1):
@@ -40,5 +41,44 @@ def svdpca(data, k=1):
     Z = evecs.T.dot(data.T)
     return Z, S
 
-def mmds(matrix, k=1):
-    
+def generateDistanceMatrix(matrix):
+    dmat = numpy.zeros_like(matrix.reshape(matrix.shape[0], matrix.shape[0]))
+    for i in range(0, matrix.shape[0]-1):
+        for j in range(i+1, matrix.shape[0]):
+            sum = 0
+            for k in range(0, matrix.shape[1]):
+                sum += math.pow((matrix[i, k] - matrix[j, k]), 2)
+            dmat[i, j] = sum
+            dmat[j, i] = sum
+    return dmat
+
+def isSymmetric(matrix):
+    for i in range(0, matrix.shape[1]):
+        for j in range(0, i+1):
+            if (matrix[j,i] != matrix[i,j]):
+                return False
+    return True
+
+def mmds(distanceMatrix):
+    size = float(distanceMatrix.shape[0])
+    #Create the Gram matrix from the distance matrix
+    massVector = numpy.full((1, size), (1/size))
+    print "\n\nmassVector\n", massVector
+    centerMatrix = numpy.identity(size) - numpy.ones((size, 1)).dot(massVector)
+    print "\n\ncenterMatrix\n", centerMatrix
+    gramMat = (-0.5)*centerMatrix.dot(distanceMatrix).dot(centerMatrix)
+    print "\n\ngramMatrix\n", gramMat
+    #Perform eigen-decomposition
+    eVals,eVecs = np.eig(gramMat)
+    print "\n\neigenVectors\n", eVecs
+    #Create diagonal matrix of eigenvalues
+    lambd = numpy.diag(eVals)
+    print "\n\nlambd\n", lambd
+    lambd = numpy.sqrt(lambd)
+    print "\n\nlambd^(1/2)\n", lambd
+    #Create euclidean points matrix
+    #scoresMat = numpy.identity(gramMat.shape[0], distanceMatrix.shape[0])
+    scoresMat = lambd
+    scoresMat = scoresMat.dot(distanceMatrix.T)
+    print "\n\n"
+    return scoresMat
